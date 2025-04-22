1fb73a94f60392a06af784821d9fe7398b41185c
xliu40 <xlinliu40@gmail.com>
2025-04-21 23:01:17 -0500

    add comments

--- main.cpp
+++ main.cpp
@@ -69,6 +69,11 @@ int main(int argc, char *argv[])
 	//
   auto start = chrono::high_resolution_clock::now();
 
+
+	// Parallellize the outer loop (each thread gets one or more rows)
+	// schedule(static) is used to assign rows to threads in a round-robin fashion
+	// reduction(+:cells) handles the counter across the threads
+	// pragma omp critical makes sure progress output isn't garbled
 	#pragma omp parallel for reduction(+:cells) schedule(static)
 	for (int r = 0; r < wm.num_rows(); r++) {
 		for (int c = 0; c < wm.num_cols(); c++) {
